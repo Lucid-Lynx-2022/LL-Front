e672a769fc961d1f7169ea1e38f6939706d5a09d
Manuel Navarro <manuelnhc@gmail.com>
2022-05-10 16:10:41 +0100

    Migrate project firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+// ANTIGUO FIREBASE
+/*export const environment = {
   firebase: {
     projectId: 'lucid-lynx-2022',
     appId: '1:1086446197346:web:102cc6d36b63431b49cf3b',
@@ -12,6 +13,18 @@ export const environment = {
     messagingSenderId: '1086446197346',
   },
   production: false
+};*/
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDntqUCafLPDkapqYKAQCV25dLQA6zs-gc",
+    authDomain: "lucid-lynx-2022b.firebaseapp.com",
+    projectId: "lucid-lynx-2022b",
+    storageBucket: "lucid-lynx-2022b.appspot.com",
+    messagingSenderId: "534827515648",
+    appId: "1:534827515648:web:fcbe223dafb774643efde5"
+  },
+  production:false
 };
 
 /*
