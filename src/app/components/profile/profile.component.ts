d0fa6dfb41d3600a21592860099c9e5a1373046c
Jorge Rivero <jrivbel@hotmail.com>
2022-05-13 19:09:03 +0100

    añadir(comprobar errores),borrar,listar

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -3,6 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { TutoService } from 'src/app/services/tuto/tuto.service';
+
+
 
 @Component({
   selector: 'app-profile',
@@ -10,10 +14,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  listTutos: any[] = [];
+  
   email: string;
   uid: string;
   displayName: string;
-  constructor(public auth: AuthService, public router: Router, private afAuth: AngularFireAuth) { }
+  tutorial: FormGroup;
+  constructor(public auth: AuthService, public router: Router, private afAuth: AngularFireAuth, private fb: FormBuilder, private tutoService: TutoService)  { 
+    this.tutorial = this.fb.group({
+      title: '',
+      description: '',
+    });
+  }
 
   ngOnInit(): void {
    
@@ -26,7 +38,43 @@ export class ProfileComponent implements OnInit {
       this.uid =user.uid
       this.displayName =user.displayName
     });
-    
+    this.getTutos();
+  }
+
+  addTutorial() {
+    console.log(this.tutorial.value);
+
+    const Tutorial: any = {
+      title: this.tutorial.get('title').value,
+      description: this.tutorial.get('description').value,
+      uid: this.uid,
+      
+      autor: this.displayName,
+      email: this.email,
+      fecha: new Date().toLocaleDateString()
+    } 
+  console.log(Tutorial);
+  }
+
+  getTutos() {
+    this.tutoService.loadTuto().then((data) => {
+      console.log(data);
+      this.listTutos = data;
+    })
+  }
+
+  deleteTuto(id: string) {
+    this.tutoService.deleteTuto(id).then((data) => {
+      console.log(data);
+      this.getTutos();
+    })
+  }
+
+  addTuto() {
+    this.tutoService.saveNewTuto(this.tutorial.get('title').value, this.tutorial.get('description').value, this.uid, this.displayName, this.email).then((data) => {
+      console.log(data);
+      this.getTutos();
+    })
   }
 
 }
---
+++ src/app/services/tuto/tuto.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+//import { Task } from './../models/task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TutoService {
+
+  urlAPI = "http://localhost:4000/publics"
+  constructor() { }
+
+
+  loadTuto(): Promise<any> {
+    return axios.get(this.urlAPI)
+      .then(response => response.data)
+  }
+
+  saveNewTuto(newTutoTitle: string, newTutoDescription: string, newTutoUid: string, newTutoAutor: string, newTutoEmail: string){
+    return axios.post(this.urlAPI, { title: newTutoTitle, description: newTutoDescription, userId: newTutoUid, displayName: newTutoAutor, email: newTutoEmail})
+      .then(response => response.data)
+
+  }
+
+
+  deleteTuto( id : string ){
+    return axios.delete(this.urlAPI + '/' + id)
+      .then(response => response.data)
+  }
+}
